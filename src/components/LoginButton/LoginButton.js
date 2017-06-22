cabce6f5e9f9501bfb547e38d7336f97bc4a7ff5
wangchao <wang504615732@gmail.com>
2017-06-22 22:32:02 +0800

    完成了LoginButton的大部分工作

--- src/components/LoginButton/LoginButton.js
+++ src/components/LoginButton/LoginButton.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 
 export default class LoginButton extends React.Component {
     static propTypes = {
-        title: PropTypes.string,
         bgColor: PropTypes.string,
+        title: PropTypes.string,
+        imgSrc: PropTypes.string,
+        imgShow: PropTypes.bool,
         onClick: PropTypes.func,
     }
+
+    static defaultProps = {
+        bgColor: 'yellow',
+        borderColor: 'yellow',
+        imgSrc: '/assets/wechat.png',
+        imgShow: true
+    }
     render() {
-        // const bgColor = this.props.bgColor
+        const styles = require('./LoginButton.scss');
+        const { borderColor } = this.props;
+        const display = this.props.imgShow ? 'inline-block' : 'none';
         return (
-            <div onClick={this.props.onClick} >
-                <img src="/assets/wechat.png" alt="wechat" />
-                <span>{this.props.title}</span>
+            <div
+                style={{ backgroundColor: this.props.bgColor, borderColor }}
+                className={styles.button}
+                onClick={this.props.onClick}
+            >
+                <img style={{ display }} className={styles.logo} src={this.props.imgSrc} alt="wechat" />
+                <span className={styles.buttonText}>{this.props.title}</span>
             </div>
         );
     }
--- src/containers/Login/container/Login.js
+++ src/containers/Login/container/Login.js
@@ -2,13 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Button } from 'antd';
+import { LoginButton } from '../../../components';
 
 export default class Login extends React.Component {
     render() {
         const styles = require('./Login.scss');
         return (
             <div className={styles.loginBack} >
-                
+                <div>asdlfkjasdfklj;asdkfj</div>
+                <Button  loading />
+                <LoginButton title="微信登陆" bgColor="transparent" borderColor="black" onClick={(e) => {console.log('click')}} />
             </div>
         );
     }
