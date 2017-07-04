76a156a92b4b3ad8cf001a5fe9fa07854346a106
wangchao <504615732@qq.com>
2017-07-04 17:13:48 +0800

    微调

--- src/containers/Register/component/StepTwo/StepTwo.js
+++ src/containers/Register/component/StepTwo/StepTwo.js
@@ -7,6 +7,9 @@ export default class StepTwo extends React.Component {
         phonenumber: PropTypes.string
     }
 
+    static defaultProps = {
+        phonenumber: ''
+    };
     constructor() {
         super();
         this.handleClickNextStep = (e) => this._handleClickNextStep(e);
@@ -25,7 +28,7 @@ export default class StepTwo extends React.Component {
             <div className={styles.steptwo}>
                 <div className={forgotpasswordStyle.description}>
                     <span>输入验证码</span>
-                    <span>{`我们向 ${this.props.phonenumber} 发送了一个短信验证码。请输入...`}</span>
+                    <span>{`我们向 ${this.props.phonenumber.replace(/(\d)(?=(\d{4})+(?!\d))/g, '$1' + ' ')} 发送了一个短信验证码。请输入...`}</span>
                 </div>
                 <div className={logingStyle.loginBottom}>
                     <Phone zone={false} title="短信验证码" onChange={e => console.log(111)} />
