f99d0bcb687cd7c61f5d0f3be24582fa7a21bc83
wangchao <504615732@qq.com>
2017-07-12 18:34:54 +0800

    微调

--- src/containers/Shoppingcart/container/Shoppingcart.js
+++ src/containers/Shoppingcart/container/Shoppingcart.js
@@ -29,11 +29,6 @@ export default class extends React.Component {
     }
 
     /**
-     *  card click event
-     */
-
-
-    /**
      *  shopping tool click event
      */
     _handleClickSettleAccount(e) {
