d02827433c6e1c8e01ca93c107126412c8c2b1ea
wangchao <504615732@qq.com>
2017-07-13 16:34:39 +0800

    login.js添加了state

--- src/containers/Login/module/login.js
+++ src/containers/Login/module/login.js
@@ -3,30 +3,34 @@ const LOGIN = 'redux/login/LOGIN'
 const SIGNUP = 'redux/login/SIGNUP'
 const WECHATLOGIN = 'redux/login/WECHATLOGIN'
 
+export function generatorAuthHeadersForUser(user) {
+    return {
+        'X-Tella-Request-AppVersion': '1.0.0',
+        'X-Tella-Request-Provider': 'web',
+        'X-Tella-Request-Timestamp': user.token_timestamp,
+        'X-Tella-Request-Token': user.access_token,
+        'X-Tella-Request-Userid': user.userid,
+        'X-Tella-Request-Device': 10
+    }
+}
+
 const actionhandlers = {
-    [`${LOGIN}_PENDING`]: (state, action) => ({
-        ...state,
-        loading: true,
-        loaded: false
-    }),
-    [`${LOGIN}_FULFILLED`]: (state, action) => {
-        console.log(action)
-        return {
-            ...state,
-            loading: false,
-            loaded: true
-        }
-    },
-    [`${LOGIN}_REJECTED`]: (state, action) => ({
-        ...state,
-        loading: false,
-        loaded: true
-    })
+    [`${LOGIN}_PENDING`]: (state, action) => ({ ...state, userLoading: true, userLoaded: false }),
+    [`${LOGIN}_FULFILLED`]: (state, action) => ({ ...state, userLoading: false, userLoaded: true, user: action.payload, authHeaders: generatorAuthHeadersForUser(action.payload) }),
+    [`${LOGIN}_REJECTED`]: (state, action) => ({ ...state, userLoading: false, userLoaded: true, userLoginError: action.payload }),
+
+    [`${SIGNUP}_PENDING`]: (state, action) => ({...state, userLoading: true, userLoaded: false }),
+    [`${SIGNUP}_FULFILLED`]: (state, action) => ({ ...state, userLoading: false, userLoaded: true, user: action.payload, authHeaders: generatorAuthHeadersForUser(action.payload) }),
+    [`${SIGNUP}_REJECTED`]: (state, action) => ({ ...state, userLoading: false, userLoaded: true, userRegisterError: action.payload }),
 };
 
 const initialState = {
-    loading: false,
-    loaded: false
+    userLoading: false,
+    userLoaded: false,
+    userLoginError: null,
+    userRegisterError: null,
+    user: null,
+    authHeaders: null
 };
 
 /**
