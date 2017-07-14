aba160a75ed940c54a3c7f1571beb019b82dd304
wangchao <wang504615732@gmail.com>
2017-07-14 22:54:57 +0800

    调整了routes

--- src/routes.js
+++ src/routes.js
@@ -15,17 +15,15 @@ const routes = (store) => {
 	return (
 		<Route path="/" component={App}>
 			<IndexRoute component={Home} />
-			<Route path="/home" component={Home} />
-			<Route path="/tickets" component={Tickets}>
-				<Route path="/:ticketType" component={Tickets} />
-			</Route>
-			<Route path="/buytickets" component={BuyTicket} />
-			<Route path="/details/:id" component={Details} />
-			<Route path="/hotdetail/:id" component={HotDetail} />
-			<Route path="/shoppingcart" component={Shoppingcart} />
-			<Route path="/login" component={Login} />
-			<Route path="/register" component={Register} />
-			<Route path="/terms/:serviceType" component={TermsOfService} />
+			<Route path="home" component={Home} />
+			<Route path="tickets" component={Tickets} />
+			<Route path="buytickets" component={BuyTicket} />
+			<Route path="details/:id" component={Details} />
+			<Route path="hotdetail/:id" component={HotDetail} />
+			<Route path="shoppingcart" component={Shoppingcart} />
+			<Route path="login" component={Login} />
+			<Route path="register" component={Register} />
+			<Route path="terms/:serviceType" component={TermsOfService} />
 			<Route path="*" component={NotFound} status={404} />
 		</Route>
 	)
