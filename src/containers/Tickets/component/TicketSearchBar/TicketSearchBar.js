0d35bc0d1871463f760b40750e32e17abe5bc8ab
wangchao <504615732@qq.com>
2017-07-14 10:33:15 +0800

    调整了搜索门票的界面

--- src/containers/Tickets/component/TicketSearchBar/TicketSearchBar.js
+++ src/containers/Tickets/component/TicketSearchBar/TicketSearchBar.js
@@ -18,7 +18,7 @@ export default class TicketSearchBar extends React.Component {
         return (
             <div className={styles.searchBar}>
                 <div className={styles.back} onClick={() => history.back()}><img src="/assets/back.png" alt="back"/></div>
-                <div className={styles.search}><Search style={{ overflow: 'hidden', borderRadius: '14px' }} placeholder="搜索门票" onSearch={this.handleSearch} /></div>
+                <div className={styles.search}><Search style={{ overflow: 'hidden', borderRadius: '14px', border: 'none' }} placeholder="搜索门票" onSearch={this.handleSearch} /></div>
             </div>
         );
     }
