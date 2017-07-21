04569681db0a475c39a639a9ae309140c4750285
wangchao <504615732@qq.com>
2017-07-21 14:42:48 +0800

    删除了utils

--- src/Utils/countDown.js
+++
@@ -1,7 +0,0 @@
-export default function countDown(count, cb) {
-     if (count < 0) return;
-     cb(count)
-     setTimeout(() => {
-         countDown(count - 1, cb)
-     }, 1000)
-}
--- src/utils/constant.js
+++
@@ -1,4 +0,0 @@
-/*
- *  cookie
- **/
-export const USER_COOKIE = 'dreampark_user_cookie'
