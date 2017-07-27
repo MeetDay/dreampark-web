7ff4b38e7985a5d86c13cf57a18c1dfa4f0c063b
wangchao <504615732@qq.com>
2017-07-27 14:06:21 +0800

    调整了字段

--- src/containers/Tickets/container/BuyTicket.js
+++ src/containers/Tickets/container/BuyTicket.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { push } from 'react-router-redux';
-import { connect } from 'react-redux';
 import { TicketCard, TicketSearchBar } from '../component';
-
-@connect(
-    state => ()
-)
 
 export default class BuyTicket extends React.Component {
     constructor(props) {
