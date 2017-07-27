6278ddabbc7e11fe81e4f7ae3a7b93696408e66f
wangchao <504615732@qq.com>
2017-07-27 15:50:18 +0800

    调整

--- src/containers/App/container/App.js
+++ src/containers/App/container/App.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { asyncConnect } from 'redux-async-connect'
 import Cookies from 'universal-cookie'
+import * as Constant from '../../../utils/constant'
 import { Navbar as NavigationBar } from '../../../components'
 import { loadCookie, isCookieLoaded } from '../../Login/module/login'
 
@@ -22,6 +23,13 @@ import { loadCookie, isCookieLoaded } from '../../Login/module/login'
 )
 
 export default class App extends React.Component {
+
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.user && nextProps.user !== this.props.user) {
+			const cookies = new Cookies()
+			cookies.setItem(Constant.USER_COOKIE, nextProps.user)
+		}
+	}
 
 	render() {
 		require('./Html.css');
