8afba92ccf7617f7789d7d2159d8f2bf17bf50e8
wangchao <504615732@qq.com>
2017-07-30 13:52:17 +0800

    添加了加载更多的组件

--- src/components/LoadMoreButton/LoadMoreButton.js
+++ src/components/LoadMoreButton/LoadMoreButton.js
@@ -3,14 +3,55 @@ import PropTypes from 'prop-types'
 
 export default class LoadMoreButton extends React.Component {
     static propTypes = {
-        loading: PropTypes.bool,
-        onClick: PropTypes.func
+        hasMore: PropTypes.bool,
+        isActive: PropTypes.bool,
+        activeDescriptionText: PropTypes.string,
+        normalDescriptionText: PropTypes.string,
+        noMoreDescriptionText: PropTypes.string,
+        onClick: PropTypes.func,
+        type: PropTypes.oneOf(['circle', 'ballspin','linespinfade']),
+    }
+    static defaultProps = {
+        type: 'circle',
+        hasMore: true,
+        isActive: false,
+        activeDescriptionText: '正在加载...',
+        normalDescriptionText: '点击加载更多...',
+        noMoreDescriptionText: '没有更多了...'
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            rotateDegree: 0,
+            clockID: null,
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const { isActive } = nextProps
+        if (isActive && isActive !== this.props.isActive) {
+            const clockID = setInterval(_ => {
+                this.setState((preState) => ({ rotateDegree: (preState.rotateDegree + 1) % 360 }))
+            }, 1000 / 360)
+            this.setState({ clockID: clockID })
+        } else {
+            clearInterval(this.state.clockID)
+        }
     }
 
     render() {
+        const styles = require('./LoadMoreButton.scss')
+        const { hasMore, isActive, activeDescriptionText, normalDescriptionText, noMoreDescriptionText } = this.props
+        const nonActiveDescriptionText = hasMore ? normalDescriptionText : noMoreDescriptionText
+        const descriptionText = isActive ? activeDescriptionText : nonActiveDescriptionText
+        const imageStyle = { transform: 'rotate(' + this.state.rotateDegree + 'deg)' };
         return (
-            <div>
-                Load More Button
+            <div className={styles.loading}>
+                <div className={styles.loadingWrap} onClick={this.props.onClick}>
+                    <img style={imageStyle} className={styles.loadingImage} src={`/loading/${this.props.type}.png`} alt="loading" />
+                    <span className={styles.loadingText}>{descriptionText}</span>
+                </div>
             </div>
         );
     }
--- src/containers/Tickets/container/BuyTicket.js
+++ src/containers/Tickets/container/BuyTicket.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { push } from 'react-router-redux';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { TicketCard, TicketSearchBar } from '../component';
+import { TicketCard, TicketSearchBar } from '../component'
+import { LoadMoreButton } from '../../../components'
 
 @connect(
     null,
@@ -14,10 +15,23 @@ export default class BuyTicket extends React.Component {
     constructor(props) {
         super(props)
         this.handleSearchFocus = (e) => this._handleSearchFocus(e)
+        this.handleClickLoadMore = (e) => this._handleClickLoadMore(e)
+        this.state = {
+            isActive: false
+        }
     }
 
     _handleSearchFocus(e) {
+        e.preventDefault()
         this.props.push('/buytickets/search')
+    }
+
+    _handleClickLoadMore(e) {
+        e.preventDefault()
+        this.setState({
+            isActive: !this.state.isActive
+        })
+        console.log('click load more...')
     }
 
     render() {
@@ -32,6 +46,7 @@ export default class BuyTicket extends React.Component {
                 <TicketCard />
                 <TicketCard />
                 <TicketCard />
+                <LoadMoreButton onClick={this.handleClickLoadMore} isActive={this.state.isActive} />
             </div>
         );
     }
