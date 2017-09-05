a11b3ece7df08785816fd13830d718e0aa6a38d5
wangchao <504615732@qq.com>
2017-09-05 18:04:33 +0800

    调整了购买酒店门票页

--- src/containers/HotDetail/component/BuyTicketNow/ticketHelper.js
+++ src/containers/HotDetail/component/BuyTicketNow/ticketHelper.js
@@ -1,8 +1,17 @@
+/**
+ * @Author: WangChao
+ * @Date:   2017-09-04T14:34:57+08:00
+ * @Email:  crazyitcoder9527@126.com
+ * @Project: dreampark-web
+ * @Last modified by:   WangChao
+ * @Last modified time: 2017-09-05T16:42:11+08:00
+ */
+
 export function ticketExisted(tickets, ticket) {
     let isExisted = false
     if (Array.isArray(tickets)) {
         const results = tickets.find((item) => item.id === ticket.id)
         if (results) isExisted = true
     }
-    return isExisted
+    return isExisted;
 }
--- src/containers/HotDetail/component/index.js
+++ src/containers/HotDetail/component/index.js
@@ -1,5 +1,15 @@
+/**
+ * @Author: WangChao
+ * @Date:   2017-09-04T14:34:57+08:00
+ * @Email:  crazyitcoder9527@126.com
+ * @Project: dreampark-web
+ * @Last modified by:   WangChao
+ * @Last modified time: 2017-09-05T14:19:06+08:00
+ */
+
 export Navbar from './Navbar/Navbar';
 export ToolBar from './ToolBar/ToolBar';
 export Recommend from './Recommend/Recommend';
 export BuyTicketNow from './BuyTicketNow/BuyTicketNow';
 export BuyParkingCoupon from './BuyParkingCoupon/BuyParkingCoupon';
+export BuyHotelTickets from './BuyHotelTickets/BuyHotelTickets';
