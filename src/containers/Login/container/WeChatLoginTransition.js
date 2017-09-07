db216f37f3b848b9a5b1c3d2ad03fc8ea093366a
wangchao <504615732@qq.com>
2017-09-08 00:01:18 +0800

    调整了微信登录

--- src/containers/Login/container/WeChatLoginTransition.js
+++ src/containers/Login/container/WeChatLoginTransition.js
@@ -54,18 +54,18 @@ export default class WeChatLoginTransition extends React.Component {
         const { user, weChatInfo, weChatInfoError } = nextProps;
 
         // 1. 微信登录成功，但登录fbpark失败, 一律跳到启动页
-        if (weChatInfo && weChatInfoError != this.props.weChatInfoError && !isEmptyObject(weChatInfoError)) {
+        if (weChatInfo && weChatInfoError && !isEmptyObject(weChatInfoError)) {
             this.props.push('/login#launching');
             return
         }
         // 2. 微信登录成功, 登录fbpark成功，但用户信息不全, 需要补全信息
-        if (weChatInfo && user != this.props.user && !isFullUser(user)) {
+        if (weChatInfo && user && !isFullUser(user)) {
             this.props.push('/register#stepthree');
             return
         }
 
         // 3. 微信登录成功, 登录fbpark成功
-        if (user != this.props.user && isFullUser(user)) {
+        if (user && isFullUser(user)) {
             const forwardUrl = sessionStorage.getItem(Constant.URL_BEFORE_LEAVE);
             this.props.push(forwardUrl || '/tickets');
             return
