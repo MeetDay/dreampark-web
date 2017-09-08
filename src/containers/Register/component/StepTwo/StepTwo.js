abd3dcfb5c19ae424a6f2ef9c931c7e999584de7
wangchao <504615732@qq.com>
2017-09-08 20:49:38 +0800

    调整了注册页的逻辑

--- src/containers/Register/component/StepTwo/StepTwo.js
+++ src/containers/Register/component/StepTwo/StepTwo.js
@@ -46,7 +46,7 @@ export default class StepTwo extends React.Component {
         }
 
         if (userSignupError && userSignupError !== this.props.userSignupError) {
-            setTimeout(_ => message.error(userSignupError.error_message), 1000)
+            setTimeout(_ => message.error(userSignupError.error_message || '注册失败'), 1000)
         }
 
         if (signUser && signUser !== this.props.signUser) {
