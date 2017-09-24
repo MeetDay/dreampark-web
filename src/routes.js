abc6efee3c0dff78723b5e93ebb10f08eed9d8f1
wangchao <504615732@qq.com>
2017-09-24 20:00:06 +0800

    调整了routes里判断已登录的情况。

--- src/routes.js
+++ src/routes.js
@@ -85,9 +85,9 @@ const routes = (store) => {
 			<Route path="user/login" component={PureLogin} />
 			<Route path="user/forgotpassword/changepassword" component={ChangePassword} />
 			<Route path="user/forgotpassword" component={ForgotPassword} />
-			<Route path="login" component={Login} />
+			<Route path="login" component={Login} onEnter={checkAlreadyLogin} />
 			<Route path="wechat" component={WeChatLoginTransition} />
-			<Route path="register" component={Register} />
+			<Route path="register" component={Register} onEnter={checkAlreadyLogin} />
 			<Route path="terms/:serviceType" component={TermsOfService} />
 			<Route path="app/download" component={Download} />
 			<Route path="*" component={NotFound} status={404} />
